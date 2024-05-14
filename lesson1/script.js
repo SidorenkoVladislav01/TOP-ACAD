669621a9f5d383765120d55f1b3521678e0df088
Vladislav <Emon.18@mail.ru>
2024-05-14 21:24:03 +1000

    test

---
+++ lesson1/script.js
@@ -0,0 +1,68 @@
+// alert( "Hello world!" );
+
+// function AlertD(){
+//     alert( "Hello world!" )
+// }
+
+// AlertD()
+// AlertD()
+// AlertD()
+
+// Объявление переменной
+
+// let message;
+
+// message = 2;
+
+// console.log(message);
+
+// let one = 1;
+// let two = 2;
+// let sum = one + two
+
+// console.log(one);
+// console.log(two);
+// console.log(sum);
+
+// let basket = 10;
+// let basket2 = basket;
+// basket2 = 2;
+
+// const COLOR_RED = "#HEX"
+// COLOR_RED = 2
+
+// Типы данных: 
+// string - строка
+// number - число
+// bigInt - число, большое
+// boolean - правда
+
+
+// let name = prompt ("Как тебя зовут");
+// let message = `Привет, ${name}`
+// // console.log(message);
+// alert(message)
+
+// let number1 = 10;
+// let number2 = 10.30
+
+// let bigInt2 = 123213213132213123n
+
+// let a = true
+// let b = false
+
+// // + 
+// - 
+// * 
+// ** - возведение в степень
+// /
+// <
+// >
+// || - или
+// && - и
+
+// let bool1 = 1 > 2
+
+// null age = null;
+
+// let age = undefined
