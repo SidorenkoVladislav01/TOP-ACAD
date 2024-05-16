1d111907abcfe1374ef5028ea6ec7934661e86d5
Vladislav <Emon.18@mail.ru>
2024-05-16 21:07:11 +1000

    test

---
+++ lesson2/script.js
@@ -0,0 +1,88 @@
+// let message;
+// message = 2;
+// alert(message);
+// message = 10;
+// alert(message)
+
+// let message;
+// message = 2;
+// let lesson = 3;
+
+// alert(message);
+// alert(lesson);
+
+// alert(message ** lesson)
+// alert(-1 / 0)
+
+// let message;
+// message = 1e6;
+// alert(message);
+
+// let message = Math.floor(3.3)
+// let message2 = Math.ceil(3.7)
+// let message3 = Math.round(3.7)
+// let message4 = Math.trunc(4.2)
+
+// alert(message4)
+
+// let num = 9007199254740992n
+
+// result1 = num + 1n
+// result2 = num + 4n
+
+// alert(result1)
+// alert(result2)
+//-------------------------------------------------------------------------------
+// СТРОКА
+
+// let string1 = "Привет"
+// let string2 = 'Привет со вторым типов ковычек'
+// let string3 = "Привет 'Игорь'"
+// let string4 = 'Привет "Игорь"'
+// alert(string1);
+// alert(string2);
+// alert(string3);
+// alert(string4);
+
+// let question = prompt("Как вас зовут?");
+// let helloMessage = `Привет ${question}`;
+// alert(helloMessage);
+//-------------------------------------------------------------------------------
+// булевый тип Boolean
+
+// let isTrue = 4 < 1;
+// alert(isTrue)
+
+// let isTrue = "abc".length == 3;
+// alert(isTrue)
+//-------------------------------------------------------------------------------
+// let age = prompt("Сколько вам лет?")
+
+// if (age > 18) {
+//     alert("Вы можете пройти")
+// }
+
+// else if (age < 18) {
+//     alert("вы не проходите")
+// }
+
+// else 
+// ------------------------------------------------------------------------------
+// let age = prompt("Сколько вам лет?")
+// alert(typeof age)
+// if (age === 18) {
+// alert("Вы можете пройти")
+// }
+// -------------------------------------------------------------------------------
+// let age = prompt("Сколько вам лет?")
+// switch(Number(age)) {
+//     case 18:
+//         alert("Вы проходите")
+//     case 19:
+//         alert("Вы не проходите")
+// }
+// ------------------------------------------------------------------------------
+// let age = prompt("Сколько вам лет?")
+// let message = age >= 18 ? "Вы проходите" : "Вы не проходите"
+// alert(message)
+//-------------------------------------------------------------------------------
