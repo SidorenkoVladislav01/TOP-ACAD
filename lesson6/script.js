05041a8fcda27a115c2654435e2446a4da3a7ae7
Vladislav <Emon.18@mail.ru>
2024-05-30 21:04:42 +1000

    lesson6

---
+++ lesson6/script.js
@@ -0,0 +1,35 @@
+// function sayHi() {
+//     alert("Hello")
+// }
+
+// alert(window.innerHeight)
+
+// document.body.style.background = "red"
+
+// setTimeout(() => document.body.style.background = '', 1000);
+// setTimeout(() => document.body.style.background = 'red', 2000);
+
+// alert(location.href)
+// location.href = "https://wikipedia.org"
+
+// alert(document.body.firstChild.textContent)
+
+// alert(document.body.parentNode)
+
+// for(let parent of document.body.parentNode) {
+//     alert(parent)
+// }
+
+// let elem = document.getElementById("elem")
+
+// elem.style.background = "red"
+
+// alert(elem)
+
+// let elem = document.querySelectorAll('p')
+// for(let e of elem){
+//     alert(e.innerHTML)
+// }
+
+// let elem = document.querySelector('.elem')
+// console.log(elem)
